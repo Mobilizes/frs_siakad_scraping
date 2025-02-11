964cb0c91b3527bcdd10946c272d18ef7194f00e
kev1nandreas <kevin.andreascn@gmail.com>
2025-02-11 23:12:27 +0700

    fix: add error handliing

--- notify.py
+++ notify.py
@@ -11,79 +11,82 @@ counter = 0
 old_mksem4 = pd.DataFrame(columns=['Kode', 'Nama MK', 'Kelas', 'Ketersediaan', 'Status'])
 
 while True:
-    # Use login credential
-    session = requests.Session()
-    session.cookies.setdefault('PHPSESSID', setup.SESSION_COOKIE)
+    try:
+        # Use login credential
+        session = requests.Session()
+        session.cookies.setdefault('PHPSESSID', setup.SESSION_COOKIE)
 
-    # Get HTML data
-    response = session.get(URL)
-    print('Status code:', response.status_code)
-    soup = BeautifulSoup(response.text, 'lxml')
+        # Get HTML data
+        response = session.get(URL)
+        print('Status code:', response.status_code)
+        soup = BeautifulSoup(response.text, 'lxml')
 
-    # Get SiAkad Real Time
-    form = soup.find_all('form')
-    tr = form[0].find_all('tr')
-    siakad_time = tr[1].find_all('td')[0].text.split('Tanggal: ')[1]
+        # Get SiAkad Real Time
+        form = soup.find_all('form')
+        tr = form[0].find_all('tr')
+        siakad_time = tr[1].find_all('td')[0].text.split('Tanggal: ')[1]
 
-    # Select data mata kuliah
-    rows = soup.find_all('table', class_='FilterBox')
-    data = rows[1].find_all('tr')
-    td_matkul = data[0].find_all('td')
-    matkul_depart = td_matkul[2].find_all('option')
+        # Select data mata kuliah
+        rows = soup.find_all('table', class_='FilterBox')
+        data = rows[1].find_all('tr')
+        td_matkul = data[0].find_all('td')
+        matkul_depart = td_matkul[2].find_all('option')
 
-    # Create list for dataframe
-    dataMK = {'Kode': [], 'Nama MK': [], 'Kelas': [], 'Ketersediaan': [], 'Status': []}
+        # Create list for dataframe
+        dataMK = {'Kode': [], 'Nama MK': [], 'Kelas': [], 'Ketersediaan': [], 'Status': []}
 
-    # Insert data to dataframe
-    for matkul in matkul_depart:
-        raw = matkul.text.split()
-        dataMK['Kode'].append(raw[0])
-        dataMK['Nama MK'].append(' '.join(raw[1:-2]))
-        dataMK['Kelas'].append(raw[-2])
-        dataMK['Ketersediaan'].append(raw[-1])
+        # Insert data to dataframe
+        for matkul in matkul_depart:
+            raw = matkul.text.split()
+            dataMK['Kode'].append(raw[0])
+            dataMK['Nama MK'].append(' '.join(raw[1:-2]))
+            dataMK['Kelas'].append(raw[-2])
+            dataMK['Ketersediaan'].append(raw[-1])
+            
+            status = raw[-1].split('/')
+            if status[0] < status[1]:
+                dataMK['Status'].append('Tersedia')
+            else:
+                dataMK['Status'].append('Penuh')
+
+        # Convert list to dataframe
+        dataMK = pd.DataFrame(dataMK)
+
+        # Data matakuliah sem 4
+        courses = [
+            "Pemrograman Jaringan",
+            "Probabilitas dan Statistik",
+            "Otomata",
+            "Manajemen Basis Data",
+            "Perancangan dan Analisis Algoritma",
+            "Pembelajaran Mesin",
+            "Perancangan Perangkat Lunak"
+        ]
+
+        # Filtering data
+        pattern = '|'.join(courses)
+        mksem4 = dataMK[dataMK['Nama MK'].str.contains(pattern)].reset_index(drop=True)
         
-        status = raw[-1].split('/')
-        if status[0] < status[1]:
-            dataMK['Status'].append('Tersedia')
+        # Looking for changes
+        num_mk = int(mksem4.shape[0])
+        changes = ['Perubahan pada mata kuliah semester 4:']
+            
+        if counter == 0:
+            old_mksem4 = mksem4
         else:
-            dataMK['Status'].append('Penuh')
+            for i in range(0, num_mk):
+                if mksem4['Ketersediaan'][i] != old_mksem4['Ketersediaan'][i]:
+                    matkul_changed = f"{mksem4['Nama MK'][i]} {mksem4['Kelas'][i]} {mksem4['Ketersediaan'][i]}"
+                    changes.append(matkul_changed)
+                    print(matkul_changed)
 
-    # Convert list to dataframe
-    dataMK = pd.DataFrame(dataMK)
-
-    # Data matakuliah sem 4
-    courses = [
-        "Pemrograman Jaringan",
-        "Probabilitas dan Statistik",
-        "Otomata",
-        "Manajemen Basis Data",
-        "Perancangan dan Analisis Algoritma",
-        "Pembelajaran Mesin",
-        "Perancangan Perangkat Lunak"
-    ]
-
-    # Filtering data
-    pattern = '|'.join(courses)
-    mksem4 = dataMK[dataMK['Nama MK'].str.contains(pattern)].reset_index(drop=True)
-    
-    # Looking for changes
-    num_mk = int(mksem4.shape[0])
-    changes = ['Perubahan pada mata kuliah semester 4:']
-        
-    if counter == 0:
-        old_mksem4 = mksem4
-    else:
-        for i in range(0, num_mk):
-            if mksem4['Ketersediaan'][i] != old_mksem4['Ketersediaan'][i]:
-                matkul_changed = f"{mksem4['Nama MK'][i]} {mksem4['Kelas'][i]} {mksem4['Ketersediaan'][i]}"
-                changes.append(matkul_changed)
-                print(matkul_changed)
-
-    # Send notification to WhatsApp
-    if len(changes) > 1:
-        msg = '\n'.join(changes)
-        pywhatkit.sendwhatmsg_instantly(setup.PHONE_NUMBER, msg, wait_time=10, tab_close=True)
-        # pywhatkit.sendwhatmsg_to_group_instantly(setup.GROUP_ID, msg, wait_time=10, tab_close=True)
+        # Send notification to WhatsApp
+        if len(changes) > 1:
+            msg = '\n'.join(changes)
+            pywhatkit.sendwhatmsg_instantly(setup.PHONE_NUMBER, msg, wait_time=10, tab_close=True)
+            # pywhatkit.sendwhatmsg_to_group_instantly(setup.GROUP_ID, msg, wait_time=10, tab_close=True)
+    except Exception as e:
+        print(e)
     
     # Wait for desired minutes
     time.sleep(setup.RELOAD_WAIT_TIME)
--- scrap.py
+++ scrap.py
@@ -7,60 +7,66 @@ import setup
 
 URL = "https://akademik.its.ac.id/list_frs.php"
 
-# Use login credential
-session = requests.Session()
-session.cookies.setdefault('PHPSESSID', setup.SESSION_COOKIE)
+try:
+    # Use login credential
+    session = requests.Session()
+    session.cookies.setdefault('PHPSESSID', setup.SESSION_COOKIE)
 
-# Get HTML data
-response = session.get(URL)
-print('Status code:', response.status_code)
-soup = BeautifulSoup(response.text, 'lxml')
+    # Get HTML data
+    response = session.get(URL)
+    print('Status code:', response.status_code)
+    soup = BeautifulSoup(response.text, 'lxml')
 
-# Get SiAkad Real Time
-form = soup.find_all('form')
-tr = form[0].find_all('tr')
-siakad_time = tr[1].find_all('td')[0].text.split('Tanggal: ')[1]
+    # Get SiAkad Real Time
+    form = soup.find_all('form')
+    tr = form[0].find_all('tr')
+    siakad_time = tr[1].find_all('td')[0].text.split('Tanggal: ')[1]
 
-# Select data mata kuliah
-rows = soup.find_all('table', class_='FilterBox')
-data = rows[1].find_all('tr')
-td_matkul = data[0].find_all('td')
-matkul_depart = td_matkul[2].find_all('option')
+    # Select data mata kuliah
+    rows = soup.find_all('table', class_='FilterBox')
+    data = rows[1].find_all('tr')
+    td_matkul = data[0].find_all('td')
+    matkul_depart = td_matkul[2].find_all('option')
 
-# Create list for dataframe
-dataMK = {'Kode': [], 'Nama MK': [], 'Kelas': [], 'Ketersediaan': [], 'Status': []}
+    # Create list for dataframe
+    dataMK = {'Kode': [], 'Nama MK': [], 'Kelas': [], 'Ketersediaan': [], 'Status': []}
 
-# Insert data to dataframe
-for matkul in matkul_depart:
-    raw = matkul.text.split()
-    dataMK['Kode'].append(raw[0])
-    dataMK['Nama MK'].append(' '.join(raw[1:-2]))
-    dataMK['Kelas'].append(raw[-2])
-    dataMK['Ketersediaan'].append(raw[-1])
-    
-    status = raw[-1].split('/')
-    if status[0] < status[1]:
-        dataMK['Status'].append('Tersedia')
-    else:
-        dataMK['Status'].append('Penuh')
+    # Insert data to dataframe
+    for matkul in matkul_depart:
+        raw = matkul.text.split()
+        dataMK['Kode'].append(raw[0])
+        dataMK['Nama MK'].append(' '.join(raw[1:-2]))
+        dataMK['Kelas'].append(raw[-2])
+        dataMK['Ketersediaan'].append(raw[-1])
+        
+        status = raw[-1].split('/')
+        if status[0] < status[1]:
+            dataMK['Status'].append('Tersedia')
+        else:
+            dataMK['Status'].append('Penuh')
 
-# Convert list to dataframe
-dataMK = pd.DataFrame(dataMK)
+    # Convert list to dataframe
+    dataMK = pd.DataFrame(dataMK)
 
-# Data matakuliah sem 4
-courses = [
-    "Pemrograman Jaringan",
-    "Probabilitas dan Statistik",
-    "Otomata",
-    "Manajemen Basis Data",
-    "Perancangan dan Analisis Algoritma",
-    "Pembelajaran Mesin",
-    "Perancangan Perangkat Lunak"
-]
+    # Data matakuliah sem 4
+    courses = [
+        "Pemrograman Jaringan",
+        "Probabilitas dan Statistik",
+        "Otomata",
+        "Manajemen Basis Data",
+        "Perancangan dan Analisis Algoritma",
+        "Pembelajaran Mesin",
+        "Perancangan Perangkat Lunak"
+    ]
 
-# Filtering data
-pattern = '|'.join(courses)
-mksem4 = dataMK[dataMK['Nama MK'].str.contains(pattern)].reset_index(drop=True)
+    # Filtering data
+    pattern = '|'.join(courses)
+    mksem4 = dataMK[dataMK['Nama MK'].str.contains(pattern)].reset_index(drop=True)
+except Exception as e:
+    print(e)
+    mksem4 = pd.DataFrame(columns=['Kode', 'Nama MK', 'Kelas', 'Ketersediaan', 'Status'])
+    dataMK = pd.DataFrame(columns=['Kode', 'Nama MK', 'Kelas', 'Ketersediaan', 'Status'])
+    siakad_time = "Error"
 
 
 # Display data
